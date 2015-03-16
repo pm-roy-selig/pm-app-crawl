3a4870fc38c4fde3f4a2163be7c0f75f98f4a75e
pm-roy-selig <roy.selig@pubmatic.com>
2015-03-16 11:27:35 -0400

    adds express server

--- test-crawl.js
+++ test-crawl.js
@@ -36,6 +36,7 @@ crawl.crawl( "http://www.pubmatic.com", function ( err, pages ) {
     );
 
     console.log( urls );
+
     capture( urls,
         { out: "./sitemaps/pubmatic-web",
             phantomBin:"/Applications/phantomjs/bin/phantomjs",
